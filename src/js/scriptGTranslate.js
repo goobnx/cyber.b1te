5bb40ebb44d61e75e45226143693712f1b436eaa
goobnx <ramaramadhan409@gmail.com>
2025-03-13 20:08:56 +0700

    script gtransalte

---
+++ src/js/scriptGTranslate.js
@@ -0,0 +1,8 @@
+window.gtranslateSettings = {
+  default_language: "id",
+  languages: ["id", "en"],
+  wrapper_selector: ".gtranslate_wrapper",
+  flag_size: 24,
+  horizontal_position: "left",
+  vertical_position: "bottom",
+};
